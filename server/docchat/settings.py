55bd436d5790f46f00c624b9ac58a2dc2dc9283c
yugansh arora <yugansharora01@gmail.com>
2025-11-29 22:40:01 +0530

    Deployment push

--- server/docchat/settings.py
+++ server/docchat/settings.py
@@ -154,6 +154,10 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 AWS_S3_VERIFY = True
 
+STATIC_URL = '/static/'
+STATIC_ROOT = '/vol/static'
+
+
 STORAGES = {
     "default": {
         "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
